70402ea15c376b6e82e7fa13de5e0e21c93b6407
BogdanPop03 <pop.bogdan01022003@gmail.com>
2023-07-13 22:21:25 +0300

    modified the game a bit

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const newGame = function () {
 	score0Element.textContent = 0
 	score1Element.textContent = 0
 
+	playerLeftScore.textContent = "Score: 0"
+	playerRightScore.textContent = "Score: 0"
+
 	diceElement.classList.add("hidden")
 
 	if (!playerLeftStatus.classList.contains("player--active")) {
 		playerLeftStatus.classList.add("player--active")
 		playerRightStatus.classList.remove("player--active")
-	} else {
-		playerRightStatus.classList.add("player--active")
-		playerLeftStatus.classList.remove("player--active")
 	}
+
+	console.log("Game was reset.")
 }
 
 const score0Element = document.getElementById("score--0")
@@ -30,6 +32,9 @@ const btnHold = document.querySelector(".btn--hold")
 const playerLeftStatus = document.querySelector(".player--0")
 const playerRightStatus = document.querySelector(".player--1")
 
+const playerLeftScore = document.getElementById("player--left--score")
+const playerRightScore = document.getElementById("player--right--score")
+
 score0Element.textContent = 0
 score1Element.textContent = 0
 
@@ -40,7 +45,7 @@ btnNew.addEventListener("click", newGame)
 btnRoll.addEventListener("click", function () {
 	const diceValue = Math.trunc(Math.random() * 6) + 1
 
-	console.log(`First dice value: ${diceValue}`)
+	console.log(`First dice value: ${diceValue}.`)
 
 	const imageName = "dice-" + String(diceValue) + ".png"
 
@@ -50,14 +55,14 @@ btnRoll.addEventListener("click", function () {
 
 	if (diceValue === 1) {
 		if (playerLeftStatus.classList.contains("player--active")) {
-			console.log("Switched from Player Left to Player Right")
+			console.log("Switched from Player Left to Player Right.")
 
 			currentScoreLeft.textContent = 0
 
 			playerLeftStatus.classList.remove("player--active")
 			playerRightStatus.classList.add("player--active")
 		} else {
-			console.log("Switched from Player Right to Player Left")
+			console.log("Switched from Player Right to Player Left.")
 
 			currentScoreRight.textContent = 0
 
@@ -77,38 +82,52 @@ btnRoll.addEventListener("click", function () {
 
 btnHold.addEventListener("click", function () {
 	if (playerLeftStatus.classList.contains("player--active")) {
-		console.log("Switched from Player Left to Player Right")
+		console.log("Switched from Player Left to Player Right.")
 
 		score0Element.textContent =
 			Number(score0Element.textContent) +
 			Number(currentScoreLeft.textContent)
 
-		if (Number(score0Element.textContent) >= 100) {
-			newGame()
+		if (Number(score0Element.textContent) >= 10) {
+			const scoreLeft =
+				Number(playerLeftScore.textContent.match(/:\s(\d+)/)[1]) + 1
+
+			playerLeftScore.textContent = `Score: ${scoreLeft}`
+
+			console.log(`Score Player Left: ${scoreLeft}.`)
+
+			// newGame()
 		}
 
 		playerLeftStatus.classList.remove("player--active")
 		playerRightStatus.classList.add("player--active")
 
 		console.log(
-			`Player left held ${currentScoreLeft.textContent} points and now has ${score0Element.textContent} points in total`
+			`Player left held ${currentScoreLeft.textContent} points and now has ${score0Element.textContent} points in total.`
 		)
 	} else {
-		console.log("Switched from Player Right to Player Left")
+		console.log("Switched from Player Right to Player Left.")
 
 		score1Element.textContent =
 			Number(score1Element.textContent) +
 			Number(currentScoreRight.textContent)
 
-		if (Number(score1Element.textContent) >= 100) {
-			newGame()
+		if (Number(score1Element.textContent) >= 10) {
+			const scoreRight =
+				Number(playerRightScore.textContent.match(/:\s(\d+)/)[1]) + 1
+
+			playerRightScore.textContent = `Score: ${scoreRight}`
+
+			console.log(`Score Player Right: ${scoreRight}.`)
+
+			// newGame()
 		}
 
 		playerRightStatus.classList.remove("player--active")
 		playerLeftStatus.classList.add("player--active")
 
 		console.log(
-			`Player right held ${currentScoreRight.textContent} points and now has ${score1Element.textContent} points in total`
+			`Player right held ${currentScoreRight.textContent} points and now has ${score1Element.textContent} points in total.`
 		)
 	}
 
